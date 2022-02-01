15324fac40282b541640909d8343b2f0a7f7480f
Markus Sibakov <markus.sibakov@myy.haaga-helia.fi>
2022-02-01 02:24:05 +0200

    ostoslista

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+
+  const addButton = () => {
+    setData([...data, {key: text}]);
+    setText('');
+  }
+
+  const clearButton = () => {
+    setData([]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={{flex:1}}>
+        <TextInput
+          style={{width: 200, borderColor:'gray', borderWidth:1 }}
+          onChangeText={input => setText(input)}
+          value={text}
+        />
+        <View style={styles.horizontal}>
+          <View style={styles.button}>
+            <Button onPress={addButton} title="add"/>
+          </View>
+          <View style={styles.button}>
+            <Button onPress={clearButton} title="clear"/>
+          </View>
+        </View>
+      </View>
+      
+      <View style={{flex:2}}>
+        <Text style={{color:'blue', fontWeight:'bold'}}>Shopping List</Text>
+        <View style={{justifyContent:'flex-start'}}>
+          <FlatList
+            data={data}
+            renderItem={ ({item}) => <Text>{item.key}</Text> }
+            keyExtractor={ (item, index) => index.toString() }
+          />
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -16,5 +54,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop:50,
   },
+  button: {
+    width:60,
+    marginTop:20,
+  },
+  horizontal: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent:'center',
+  }
 });
